6fd437703f32860ab167323f8622e97027187421
mohammedaladhary <caladhary@gmail.com>
2023-06-07 11:56:45 +0400

    switch-recursion

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("enter a number:");
+        //for (int i = 1; i <= 7; i++)
+        //{
+        //    int a = int.Parse(Console.ReadLine());
+        //    int day = a;
+
+
+        //    switch (a)
+        //    {
+        //        case 1:
+        //            Console.WriteLine("Today is Sunday.");
+        //            break;
+        //        case 2:
+        //            Console.WriteLine("Today is Monday.");
+        //            break;
+        //        case 3:
+        //            Console.WriteLine("Today is Tuesday.");
+        //            break;
+        //        case 4:
+        //            Console.WriteLine("Today is Wednesday.");
+        //            break;
+        //        case 5:
+        //            Console.WriteLine("Today is Thursday.");
+        //            break;
+        //        default:
+        //            Console.WriteLine("Looking forward to the Weekend.");
+        //            break;
+        //    }
+        //}
+
+
+
+    }
+}
