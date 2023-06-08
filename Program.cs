9c6e88938b43660ca790a0d98df3941ce8f6981a
mohammedaladhary <caladhary@gmail.com>
2023-06-08 10:56:43 +0400

    powerRecursion

--- Program.cs
+++ Program.cs
@@ -1,13 +1,50 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
+    static int power(int number, int pow)
+    {
+        int value = number;
+        for (int i = 2; i <= pow; i++)
+        {
+            value = value * number;
+        }
+        return value;
+    }
+
+    static int powerRecursion(int num, int pow)
+    {
+        if (num == 0)
+        {
+            return 1;
+        }
+        else if (pow == 1)
+        {
+            return num;
+        }
+        return num * powerRecursion(num, pow - 1);
+    }
     private static void Main(string[] args)
     {
-        //Console.WriteLine("enter a number:");
-        //for (int i = 1; i <= 7; i++)
-        //{
-        //    int a = int.Parse(Console.ReadLine());
-        //    int day = a;
 
+        Console.WriteLine("enter a number:");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("enter a power:");
+        int pow = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("The result is: "+powerRecursion(num,pow));
+
+        System.Console.WriteLine(powerRecursion(1, 2));
+        System.Console.WriteLine(powerRecursion(3, 3));
+        Console.WriteLine("-------------------------");
+        System.Console.WriteLine(power(1, 2));
+        System.Console.WriteLine(power(3, 3));
+
+
+        //Console.WriteLine("enter a number:");
+        //int a = int.Parse(Console.ReadLine());
+
+        //    if (a >= 1 && a <= 7) {
 
         //    switch (a)
         //    {
@@ -26,13 +63,18 @@
         //        case 5:
         //            Console.WriteLine("Today is Thursday.");
         //            break;
-        //        default:
-        //            Console.WriteLine("Looking forward to the Weekend.");
+        //        case 6:
+        //            Console.WriteLine("Today is Friday.");
+        //            break;
+        //        case 7:
+        //            Console.WriteLine("Today is Saturday.");
         //            break;
         //    }
         //}
-
-
+        //    else { 
+        //    Console.WriteLine("Try Again later"); 
+        //}
 
     }
+
 }
